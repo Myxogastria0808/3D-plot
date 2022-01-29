4fec445e851844c110c726c71d973de4efb985ab
Myxogastria0808 <78744619+Myxogastria0808@users.noreply.github.com>
2022-01-29 23:11:36 +0900

    second push

--- rgl.r
+++ rgl.r
@@ -9,5 +9,7 @@ c <-rnorm(100, 100, 10)
 
 df <-data.frame("a"=a, "b"=b, "c"=c)
 
+#This is a type h plot.
 plot3d(df[c("a", "b", "c")], type="h", xlab="X", ylab="Y", zlab="Z", col = rainbow(3))
+#This is a type p plot.
 plot3d(df[c("a", "b", "c")], type="p", xlab="X", ylab="Y", zlab="Z", col = rainbow(3))
